4bf8d755a581ac7ca5ff202429d4273cce9ad6c4
Simon Taylor <staylor@ifarm1402.jlab.org>
2019-03-22 10:17:41 -0400

    Make cdc matching cuts parameters

--- src/libraries/TRACKING/DTrackFinder.cc
+++ src/libraries/TRACKING/DTrackFinder.cc
@@ -7,12 +7,6 @@
 
 #include "DTrackFinder.h"
 
-#define CDC_MATCH_RADIUS 4.0
-#define CDC_AXIAL_MATCH_RADIUS 10.0
-#define CDC_COSMIC_MATCH_PHI 0.35
-#define CDC_MATCH_PHI 0.04
-
-
 bool DTrackFinder_cdc_hit_cosmics_cmp(const DCDCTrackHit *a,const DCDCTrackHit *b){
    return(a->wire->origin.Y()>b->wire->origin.Y());
 }
@@ -42,10 +36,19 @@ DTrackFinder::DTrackFinder()
    VERBOSE=0;
    gPARMS->SetDefaultParameter("TRKFIND:VERBOSE", VERBOSE);
 
+   CDC_MATCH_RADIUS=2.5;
+   gPARMS->SetDefaultParameter("TRKFIND:CDC_MATCH_RADIUS", CDC_MATCH_RADIUS);
+
+   CDC_MATCH_PHI=0.04;
+   gPARMS->SetDefaultParameter("TRKFIND:CDC_MATCH_PHI", CDC_MATCH_PHI);
+   
+   CDC_COSMIC_MATCH_PHI=0.35;
+   gPARMS->SetDefaultParameter("TRKFIND:CDC_COSMIC_MATCH_PHI", CDC_COSMIC_MATCH_PHI);
+
    if (DEBUG_HISTS){
       hCDCMatch_PairD = new TH1F("CDC Pair distance", "CDC Pair distance", 100, 0.0, 20.0);
-      hCDCMatch_Axial = new TH1F("CDC Match Axial", "CDC Match Axial", 100, 0.0, 20.0);
-      hCDCMatch_Stereo = new TH1F("CDC Match Stereo", "CDC Match Stereo", 100, 0.0, 20.0);
+      hCDCMatch_Axial = new TH1F("CDC Match Axial", "CDC Match Axial", 360, -M_PI_2, M_PI_2);
+   
       hFDCLayerRaw = new TH1I("FDC Layer Raw", "Layer of FDC hit before track finding", 24, 0.5 , 24.5);
       hFDCLayer = new TH1I("FDC Layer", "Layer of FDC hit after segment finding", 24, 0.5 , 24.5);
       hFDCLayerFirst = new TH1I("First FDC Layer", "First Layer of FDC segment finding", 24, 0.5 , 24.5);
@@ -217,6 +220,9 @@ bool DTrackFinder::LinkCDCSegments(void){
                while (dphi>M_PI) dphi-=2*M_PI;
                while (dphi<-M_PI) dphi+=2*M_PI;
                double matchphi = CDC_MATCH_PHI;
+	       if (DEBUG_HISTS){
+		 hCDCMatch_Axial->Fill(dphi);
+	       }
                if (COSMICS) matchphi = CDC_COSMIC_MATCH_PHI;
                if ( fabs(dphi) < matchphi){
                   axial_segments[j].matched=true;
@@ -369,10 +375,12 @@ jerror_t DTrackFinder::cdc_track_t::FindStateVector(bool IsCosmics){
          sumyz+=y*z;
       }
    }
+   /*
    if (IsCosmics==false){  // Encourage track to come from target region
-     sumz+=65.;
+     sumz+=TARGET_Z;
      sumv+=1.;
    }
+   */
    const double EPS=1e-4;
    double xdenom=sumv*sumxz-sumx*sumz;
    if (fabs(xdenom)<EPS) return VALUE_OUT_OF_RANGE;
--- src/libraries/TRACKING/DTrackFinder.h
+++ src/libraries/TRACKING/DTrackFinder.h
@@ -150,6 +150,7 @@ class DTrackFinder:public jana::JObject{
       vector<fdc_segment_t>fdc_tracks;
 
       bool COSMICS, DEBUG_HISTS, VERBOSE;
+      double CDC_MATCH_RADIUS,CDC_MATCH_PHI,CDC_COSMIC_MATCH_PHI;
 };
 
 #endif // _DTrackFinder_
