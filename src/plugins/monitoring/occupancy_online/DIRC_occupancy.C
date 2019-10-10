85c704c2c7a8d47c768a6da431f7450f8464ebd1
Justin Stevens <jrsteven@jlab.org>
2019-10-09 22:15:39 -0400

    add north box to DIRC_occupancy macros

--- src/plugins/monitoring/occupancy_online/DIRC_occupancy.C
+++ src/plugins/monitoring/occupancy_online/DIRC_occupancy.C
@@ -3,9 +3,7 @@
 // which histograms to fetch for the macro.
 //
 // hnamepath: /occupancy/dirc_num_events
-// hnamepath: /occupancy/dirc_tdc_pixel_N_occ
 // hnamepath: /occupancy/dirc_tdc_pixel_S_occ
-// hnamepath: /occupancy/dirc_tdc_pixel_N_occ_led
 // hnamepath: /occupancy/dirc_tdc_pixel_S_occ_led
 //
 // e-mail: davidl@jlab.org
@@ -17,14 +15,12 @@
 	// RootSpy saves the current directory and style before
 	// calling the macro and restores it after so it is OK to
 	// change them and not change them back.
-   TDirectory *savedir = gDirectory;
+   	TDirectory *savedir = gDirectory;
 	TDirectory *dir = (TDirectory*)gDirectory->FindObjectAny("occupancy");
 	if(dir) dir->cd();
 
-	TH2I *dirc_tdc_pixel_N_occ_led = (TH2I*)gDirectory->FindObjectAny("dirc_tdc_pixel_N_occ_led");
 	TH2I *dirc_tdc_pixel_S_occ_led = (TH2I*)gDirectory->FindObjectAny("dirc_tdc_pixel_S_occ_led");
-	TH2I *dirc_tdc_pixel_N_occ = (TH2I*)gDirectory->FindObjectAny("dirc_tdc_pixel_N_occ");
-   TH2I *dirc_tdc_pixel_S_occ = (TH2I*)gDirectory->FindObjectAny("dirc_tdc_pixel_S_occ");
+   	TH2I *dirc_tdc_pixel_S_occ = (TH2I*)gDirectory->FindObjectAny("dirc_tdc_pixel_S_occ");
 	TH1I *dirc_num_events = (TH1I*)gDirectory->FindObjectAny("dirc_num_events");
 
 	double Nevents = 1.0;
