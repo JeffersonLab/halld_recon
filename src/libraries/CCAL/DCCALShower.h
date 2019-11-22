b182372395bf6253ad4d8de352e4d6d9277e8798
andrsmit <andrsmit@jlab.org>
2019-11-22 15:11:36 -0500

    Replace the existing island fortran code with a c++ version that doesn't use common blocks.

--- src/libraries/CCAL/DCCALShower.h
+++ src/libraries/CCAL/DCCALShower.h
@@ -11,7 +11,7 @@
 
 #include <DVector3.h>
 #include "DCCALHit.h"
-#include "hycal.h"
+#include "ccal.h"
 
 using namespace std;
 
@@ -21,56 +21,101 @@ using namespace jana;
 
 #define CCAL_USER_HITS_MAX  1000
 
-class DCCALShower : public JObject {
-  public:
-      JOBJECT_PUBLIC(DCCALShower);
-      
-      DCCALShower();
-      ~DCCALShower();
-      
-      
-      int type= 0;
-      int dime= 0;
-      int status= 0;
-      int id= 0;
-      int idmax= 0;
 
-      double E= 0;
-      double x= 0;
-      double y= 0;
-      double z= 0;
-      double x1= 0;
-      double y1= 0;
-      double chi2= 0;
-      double sigma_E= 0;
-      double Emax= 0;
-      double time= 0;
-      double sigma_t= 0;
+class DCCALShower : public JObject {
+  	
+	public:
+      		JOBJECT_PUBLIC(DCCALShower);
       
-      int id_storage[MAX_CC] = {0};
-      double en_storage[MAX_CC] {0.};
-      double t_storage[MAX_CC] = {0.};
+      		DCCALShower();
+      		~DCCALShower();
       
-      void toStrings(vector<pair<string,string> > &items) const {
-	AddString(items, "E(GeV)",    "%2.3f",  E);
-	AddString(items, "Emax(GeV)", "%2.3f",  Emax);
-	AddString(items, "x(cm)",     "%3.1f",  x);
-	AddString(items, "y(cm)",     "%3.1f",  y);
-	AddString(items, "z(cm)",     "%3.1f",  z);
-	AddString(items, "x1(cm)",    "%3.1f",  x1);
-	AddString(items, "y1(cm)",    "%3.1f",  y1);
-	AddString(items, "chi2",      "%3.1f",  chi2);
-	AddString(items, "type",      "%3d",    type);
-	AddString(items, "dime",      "%3d",    dime);
-	AddString(items, "status",    "%3d",    status);
-	AddString(items, "id",        "%3d",    id);
-	AddString(items, "idmax",     "%3d",    idmax);
-	AddString(items, "sigma_E",   "%3.1f",  sigma_E);
-	AddString(items, "t(ns)",     "%2.3f",  time);
-      }
+      		/*----------------------------------------//
       
- private:
-      
+      		Description of DCCALShower variables:
+      		
+      		- E:       Energy of shower as reconstructed from island algorithm
+      		- Esum:    Energy of shower calculated as simple sum of block energies
+      		
+      		- x , y :  Shower position reported from island algorithm
+      		- x1, y1:  Shower position calculated using logarithmic weights
+      		- z:       Z-Position of CCAL Face
+      		
+      		- dime:    Number of hits contributing to shower
+      		- idmax:   The channel number of cell with maximum energy deposition
+      		- id:      Only non-zero for showers that share a single peak
+			
+      		- chi2:    The chi2-value reported from island
+      		- sigma_E: Energy resolution (needs work)
+      		- Emax:    Energy of maximum cell
+      		- time:    Energy-weighted average of shower's constituent times 
+				(with time-walk correction)
+      		
+      		- status (enum):
+      		
+        	  kInner  - shower is in most inner layer (under absorber)
+		  kMiddle - shower is in middle layers of calorimeter
+		  kOuter  - shower is in most outer layer of calorimeter
+      		
+      		- ClusterType (enum):
+        	
+		  SinglePeak - shower is in a single-peak cluster
+		  MultiPeak  - shower is in a multi-peak cluster
+		
+      		- PeakType (enum) :
+      		
+        	  OneGamma  - The peak this shower belongs to has just one gamma reconstructed
+		  TwoGammas - The peak this shower belongs to has two gammas reconstructed
+		
+		
+      		
+      		//----------------------------------------*/
+		
+      		double E;
+      		double Esum;
+      		
+      		double x;
+      		double y;
+      		double x1;
+      		double y1;
+      		double z;
+      		
+      		double chi2;
+      		double sigma_E;
+      		double Emax;
+      		double time;
+      		double sigma_t;
+      		
+      		int dime;
+      		int idmax;
+      		int id;
+      		int type;
+		
+		ClusterType_t ClusterType;
+		PeakType_t PeakType;
+      		
+		
+      		void toStrings(vector<pair<string,string> > &items) const {
+		  AddString(items, "E(GeV)",      "%2.3f",  E);
+		  AddString(items, "Emax(GeV)",   "%2.3f",  Emax);
+		  AddString(items, "x(cm)",       "%3.3f",  x);
+		  AddString(items, "y(cm)",       "%3.3f",  y);
+		  AddString(items, "x1(cm)",      "%3.3f",  x1);
+		  AddString(items, "y1(cm)",      "%3.3f",  y1);
+		  AddString(items, "z(cm)",       "%3.3f",  z);
+		  AddString(items, "chi2",        "%3.3f",  chi2);
+		  AddString(items, "dime",        "%3d",    dime);
+		  AddString(items, "idmax",       "%3d",    idmax);
+		  AddString(items, "id",          "%3d",    id);
+		  AddString(items, "sigma_E",     "%3.1f",  sigma_E);
+		  AddString(items, "t(ns)",       "%2.3f",  time);
+		  AddString(items, "ClusterType", "%d",     (int)ClusterType);
+		  AddString(items, "PeakType",    "%d",     (int)PeakType);
+      		}
+      		
+ 	
+	private:
+      	
 };
 
 
