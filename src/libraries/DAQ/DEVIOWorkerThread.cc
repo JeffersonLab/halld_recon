10b0e929595d2d69e4c0726ec24ee76754d94e15
faustus123 <davidl@jlab.org>
2019-12-11 07:35:33 -0500

    Add some extra debug info when bad f250 pulse data encountered.

--- src/libraries/DAQ/DEVIOWorkerThread.cc
+++ src/libraries/DAQ/DEVIOWorkerThread.cc
@@ -1500,7 +1500,11 @@ void DEVIOWorkerThread::Parsef250Bank(uint32_t rocid, uint32_t* &iptr, uint32_t 
 					
 					while( (*++iptr>>31) == 0 ){
 					
-						if( (*iptr>>30) != 0x01) { jerr << "Bad f250 Pulse Data for rocid="<<rocid<<" slot="<<slot<<" channel="<<channel<<endl; throw JException("Bad f250 Pulse Data!", __FILE__, __LINE__);}
+						if( (*iptr>>30) != 0x01) {
+							jerr << "Bad f250 Pulse Data for rocid="<<rocid<<" slot="<<slot<<" channel="<<channel<<endl;
+							DumpBinary(&iptr[-2], iend, 128, iptr);
+							throw JException("Bad f250 Pulse Data!", __FILE__, __LINE__);
+						}
  
 						// from word 2
 						uint32_t integral                  = (*iptr>>12) & 0x3FFFF;
@@ -1511,7 +1515,10 @@ void DEVIOWorkerThread::Parsef250Bank(uint32_t rocid, uint32_t* &iptr, uint32_t 
 						if(VERBOSE>7) cout << "      FADC250 Pulse Data word 2(0x"<<hex<<*iptr<<dec<<")  integral="<<integral<<endl;
 
 						iptr++;
-						if( (*iptr>>30) != 0x00) throw JException("Bad f250 Pulse Data!", __FILE__, __LINE__);
+						if( (*iptr>>30) != 0x00){
+							DumpBinary(&iptr[-3], iend, 128, iptr);
+							throw JException("Bad f250 Pulse Data!", __FILE__, __LINE__);
+						}
  
 						// from word 3
 						uint32_t course_time               = (*iptr>>21) & 0x1FF;//< 4 ns/count
