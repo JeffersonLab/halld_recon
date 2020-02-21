e1f1743bc3cf139070dacb17365036662e2f59ca
Simon Taylor <staylor@ifarm1901.jlab.org>
2020-02-21 14:27:38 -0500

    remove fake point at origin in Method4

--- src/libraries/TRACKING/DTrackCandidate_factory.cc
+++ src/libraries/TRACKING/DTrackCandidate_factory.cc
@@ -1747,8 +1747,7 @@ bool DTrackCandidate_factory::MatchMethod4(const DTrackCandidate *srccan,
 	    
 	  // Create a new DHelicalFit object for fitting combined data
 	  DHelicalFit fit; 
-	  // Fake point at origin
-	  fit.AddHitXYZ(0.,0.,TARGET_Z,BEAM_VAR,BEAM_VAR,0.,true); 
+	
 	  // Add hits to the fit object and also to the track candidate
 	  // itself as associated objects
 	  for (unsigned int m=0;m<src_segments.size();m++){
