0fd5d22191f4aa125be91ba60276c91b20d916f1
Richard Jones <rjones30@gmail.com>
2020-07-08 20:32:20 -0400

    * introduce new data class DEventHitStatistics for including hits   statistics summary information in the REST file. [rtj]

--- src/libraries/HDDM/HDDM_init.cc
+++ src/libraries/HDDM/HDDM_init.cc
@@ -3,12 +3,14 @@ using namespace jana;
 
 #include "DEventWriterREST_factory.h"
 #include "DEventWriterHDDM_factory.h"
+#include "DEventHitStatistics_factory.h"
 
 jerror_t HDDM_init(JEventLoop *loop)
 {
 	/// Create and register HDDM data factories
-        loop->AddFactory(new DEventWriterREST_factory);
-	loop->AddFactory(new DEventWriterHDDM_factory);
+    loop->AddFactory(new DEventWriterREST_factory());
+	loop->AddFactory(new DEventWriterHDDM_factory());
+	loop->AddFactory(new DEventHitStatistics_factory());
 
 	return NOERROR;
 }
