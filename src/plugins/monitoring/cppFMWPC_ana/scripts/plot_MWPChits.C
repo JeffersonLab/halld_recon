8b28d8bb363f9017cfba5f8f66dc960bd8b72584
Elton Smith <elton@jlab.org>
2021-06-21 16:45:27 -0400

    add script to plot additional plots using cppFMWPC_ana histograms

--- src/plugins/monitoring/cppFMWPC_ana/scripts/plot_MWPChits.C
+++ src/plugins/monitoring/cppFMWPC_ana/scripts/plot_MWPChits.C
@@ -1,4 +1,4 @@
-void plot_MWPChits (TString tag="Circle_dead")
+void plot_MWPChits (TString tag="070_pi_plus")
 {
 // File: plot_MWPChits.C
     // Read output of cppMWPC plugin and plot histograms.
@@ -16,10 +16,15 @@ void plot_MWPChits (TString tag="Circle_dead")
     TString index;
     
     // TFile *inData = new TFile("hd_root_particle_gun_031007_000.root","read");
-    // TFile *inData = new TFile("hd_root_particle_gun_031009_000.root","read");  // 2deg +/-0.1 deg
+    // TFile *inData = new TFile("hd_root_particle_gun_031009_000.root","read");  // 2deg +/-0.1 deg. 8 GeV muons
     // TFile *inData = new TFile("hd_root_particle_gun_031011_000.root","read");
-    // TFile *inData = new TFile("hd_root_particle_gun_031012_000.root","read");    // 2deg +/- 3.5 deg
-    TFile *inData = new TFile("hd_root_particle_gun_031018.root","read");    // 2deg +/- 3.95 deg; Dead wires
+    // TFile *inData = new TFile("hd_root_particle_gun_031012_000.root","read");    // 2deg +/- 3.5/2 deg
+    // TFile *inData = new TFile("hd_root_particle_gun_031018.root","read");    // 2deg +/- 3.95/2 deg; Dead wires
+    // TFile *inData = new TFile("hd_root_particle_gun_031021_"+tag+".root","read");    // Muons: 2deg +/- 3.95/2 deg; Dead wires; 0.5-7.5 GeV muons
+    // TFile *inData = new TFile("hd_root_particle_gun_031022_"+tag+".root","read");    // pions-: 2deg +/- 3.95/2 deg; Dead wires; 0.5-7.5 GeV pions
+    // TFile *inData = new TFile("hd_root_particle_gun_031023_"+tag+".root","read");    // pions+: 2deg +/- 3.95/2 deg; Dead wires; 0.25-5.75 GeV pions
+    // TFile *inData = new TFile("hd_root_particle_gun_031024_"+tag+".root","read");    // pions+: 2deg +/- 3.95/2 deg; Dead wires; 0.25-5.75 GeV muons
+    TFile *inData = new TFile("hd_root_particle_gun_031024_"+tag+".root","read");    // pions+: 2deg +/- 3.95/2 deg; Dead wires; 0.25-5.75 GeV pions
     TDirectory *dir = (TDirectory *)gDirectory->FindObjectAny("FMWPC");
     if (dir) dir->cd();
     
@@ -28,6 +33,11 @@ void plot_MWPChits (TString tag="Circle_dead")
     TH2D *FMWPCwiresE[nFMWPCchambers];
     TH1D *FMWPCwiresT_py[nFMWPCchambers];   // time projection
     TH1D *FMWPCwiresE_py[nFMWPCchambers];   // energy projection
+    TH2D *h2_pmuon_vs_mult[nFMWPCchambers];  // multiplicity 2D plots
+    
+    Int_t nslices=12;
+    TH1D *h1_[nslices];
+    
     
     for (Int_t jj=0; jj<nFMWPCchambers; jj++) {
         index.Form("FMWPCwiresT%d",jj);
@@ -36,6 +46,9 @@ void plot_MWPChits (TString tag="Circle_dead")
         index.Form("FMWPCwiresE%d",jj);
         cout << "jj=" << jj << "index=" << index << endl;
         FMWPCwiresE[jj] = (TH2D*) gDirectory->FindObjectAny(index);
+        index.Form("h2_pmuon_vs_mult%d",jj);
+        cout << "jj=" << jj << "index=" << index << endl;
+        h2_pmuon_vs_mult[jj] = (TH2D*) gDirectory->FindObjectAny(index);
     }
     
     TH2D *h2_V1_vs_H2 = (TH2D*) gDirectory->FindObjectAny("h2_V1_vs_H2");
@@ -47,6 +60,8 @@ void plot_MWPChits (TString tag="Circle_dead")
     TH2D *h2_H2_vs_H4 = (TH2D*) gDirectory->FindObjectAny("h2_H2_vs_H4");
     TH2D *h2_V3_vs_V5 = (TH2D*) gDirectory->FindObjectAny("h2_V3_vs_V5");
     TH2D *h2_V5_vs_V6 = (TH2D*) gDirectory->FindObjectAny("h2_V5_vs_V6");
+    
+    TH2D *h2_pmuon_vs_MWPC = (TH2D*) gDirectory->FindObjectAny("h2_pmuon_vs_MWPC");
     
     Double_t xmin=0.;
     Double_t xmax=2.0;
@@ -306,6 +321,68 @@ void plot_MWPChits (TString tag="Circle_dead")
         t1->DrawLatex(0.3,0.75,text);    }
     
     
+    TCanvas *c3 = new TCanvas("c3", "c3",200,10,1300,700);
+    gPad->SetGridx();
+    gPad->SetGridy();
+    // gPad->SetLogz();
+    // h2_pmuon_vs_MWPC->SetTitle("");
+    // h2_pmuon_vs_MWPC->GetXaxis()->SetRangeUser(xmin,xmax);
+    // h2_pmuon_vs_MWPC->GetYaxis()->SetRangeUser(ymin,ymax);
+    h2_pmuon_vs_MWPC->GetXaxis()->SetTitleSize(0.05);
+    h2_pmuon_vs_MWPC->GetYaxis()->SetTitleSize(0.05);
+    h2_pmuon_vs_MWPC->GetYaxis()->SetTitle("Generated momentum (GeV)");
+    h2_pmuon_vs_MWPC->GetXaxis()->SetTitle("MWPC plane number");
+    h2_pmuon_vs_MWPC->SetLineColor(2);
+    h2_pmuon_vs_MWPC->SetMarkerColor(2);
+    h2_pmuon_vs_MWPC->SetMarkerStyle(20);
+    h2_pmuon_vs_MWPC->SetMarkerSize(0.5);
+    h2_pmuon_vs_MWPC->Draw("colz");
+    
+    Double_t DeltaE[nFMWPCchambers]={0.38,0.52,0.73,1.23,1.72,1.72};
+    Double_t nMWPC[nFMWPCchambers]={1.5,2.5,3.5,4.5,5.5,6.5};
+    
+    TGraph *eloss = new TGraph(nFMWPCchambers,nMWPC,DeltaE);
+    
+    eloss->SetMarkerStyle(33);
+    eloss->SetMarkerColor(2);
+    eloss->SetMarkerSize(2.0);
+    eloss->Draw("psame");
+    
+    c3->SaveAs("plot_MWPChits_"+tag+"_c3.png");
+
+    TCanvas *c4 = new TCanvas("c4", "c4",200,10,1300,700);
+    c4->Divide(nFMWPCchambers,2);
+    
+    for (Int_t jj=0; jj<nFMWPCchambers; jj++) {
+        c4->cd(jj+1);
+        gPad->SetLogz();
+        // gPad->SetGridx();
+        // gPad->SetGridy();
+        text.Form(tag+" MWPC #%d",jj+1);
+         h2_pmuon_vs_mult[jj]->SetTitle(text);
+        // h2_pmuon_vs_mult[jj]->GetXaxis()->SetRangeUser(xmin,xmax);
+        // h2_pmuon_vs_mult[jj]->GetYaxis()->SetRangeUser(ymin,ymax);
+        h2_pmuon_vs_mult[jj]->GetXaxis()->SetTitleSize(0.05);
+        h2_pmuon_vs_mult[jj]->GetYaxis()->SetTitleSize(0.05);
+        h2_pmuon_vs_mult[jj]->GetXaxis()->SetTitle("Multiplicity");
+        h2_pmuon_vs_mult[jj]->GetYaxis()->SetTitle("Momentum (GeV)");
+        h2_pmuon_vs_mult[jj]->SetLineColor(2);
+        h2_pmuon_vs_mult[jj]->SetMarkerColor(2);
+        h2_pmuon_vs_mult[jj]->SetMarkerStyle(20);
+        h2_pmuon_vs_mult[jj]->SetMarkerSize(0.5);
+        h2_pmuon_vs_mult[jj]->Draw("colz");
+        
+        /*text.Form("MWPC #%d",jj+1);
+        t1->SetNDC();
+        t1->SetTextSize(0.07);
+        t1->DrawLatex(0.3,0.8,text);
+        text.Form("Drift Times");
+        t1->SetNDC();
+        t1->SetTextSize(0.07);
+        t1->DrawLatex(0.3,0.75,text);*/
+    }
+    
+    
     /*TLegend *leg = new TLegend(0.6,0.25,0.85,0.4);
     leg->AddEntry(h2_V1_vs_H2,"Data","p");
     leg->Draw();
@@ -315,8 +392,11 @@ void plot_MWPChits (TString tag="Circle_dead")
     t1->DrawLatex(0.3,0.40,tag);*/
     
     
+    
     c0->SaveAs("plot_MWPChits_"+tag+".pdf(");
     c1->SaveAs("plot_MWPChits_"+tag+".pdf");
-    c2->SaveAs("plot_MWPChits_"+tag+".pdf)");
+    c2->SaveAs("plot_MWPChits_"+tag+".pdf");
+    c3->SaveAs("plot_MWPChits_"+tag+".pdf");
+    c4->SaveAs("plot_MWPChits_"+tag+".pdf)");
     
 }
