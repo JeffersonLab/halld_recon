dd2d1eb41f8f5408a7d76f35ff4eb69415b83fa9
Naomi Jarvis <nsj@cmu.edu>
2021-08-05 16:56:11 -0400

    Added HistMacro script and made the root tree optional

--- src/plugins/monitoring/fa125_itrig/JEventProcessor_fa125_itrig.h
+++ src/plugins/monitoring/fa125_itrig/JEventProcessor_fa125_itrig.h
@@ -26,6 +26,8 @@ class JEventProcessor_fa125_itrig:public jana::JEventProcessor{
 		jerror_t evnt(jana::JEventLoop *eventLoop, uint64_t eventnumber);	///< Called every event.
 		jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
 		jerror_t fini(void);						///< Called after last event of last event source has been processed.
+
+                bool MAKE_TREE;
 };
 
 #endif // _JEventProcessor_fa125_itrig_
