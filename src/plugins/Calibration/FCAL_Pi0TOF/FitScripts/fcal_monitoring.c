a48ebb1df7bbcf1e37a5a7442bb3ade45298f3b0
igjaegle <igjaegle@gmail.com>
2021-10-11 18:11:10 -0400

    update macro

--- src/plugins/Calibration/FCAL_Pi0TOF/FitScripts/fcal_monitoring.c
+++ src/plugins/Calibration/FCAL_Pi0TOF/FitScripts/fcal_monitoring.c
@@ -117,11 +117,12 @@ void fcal_monitoring() {
   for (int i = 0; i < 2800; i ++) channelNum[i] = i + 1;
 
   TProfile * hCurrentGainConstants = (TProfile *) gDirectory->Get("FCAL_Pi0/CurrentGainConstants");
-  TString hname[] = {"XY/xy_all", "XY/xy_1GeV", "XY/xy_2GeV", "XY/xy_3GeV", "XY/xy_4GeV", "XY/xy_5GeV", "XY/xy_6GeV", "XY/xy_7GeV", "XY/xy_8GeV", "XY/xy_9GeV", "XY/xy_10GeV"};
-  TH2F * h2D[11];
-  for (int i = 0; i < 12; i ++) {
-    if (i < 11) h2D[i] = (TH2F *) gDirectory->Get(hname[i]);
-    else h2D[i] = (TH2F *) gDirectory->Get(hname[3]);
+  //TString hname[] = {"XY/xy_all", "XY/xy_1GeV", "XY/xy_2GeV", "XY/xy_3GeV", "XY/xy_4GeV", "XY/xy_5GeV", "XY/xy_6GeV", "XY/xy_7GeV", "XY/xy_8GeV", "XY/xy_9GeV", "XY/xy_10GeV"};
+  TString hname[] = {"XY/xy_all", "XY/xy_3GeV"};
+  TH2F * h2D[3];
+  for (int i = 0; i < 3; i ++) {
+    if (i < 2) h2D[i] = (TH2F *) gDirectory->Get(hname[i]);
+    else h2D[i] = (TH2F *) gDirectory->Get(hname[1]);
     Nice3D2D(h2D[i], 0.05, 0.05, 42, 505, 1.1, 0.9, 1.25, "",
 	     "#font[42]{x [cm]}",
 	     "#font[42]{y [cm]}",
@@ -133,9 +134,11 @@ void fcal_monitoring() {
     smallBetween4 = .125;
     
     if (i == 0) cleg = "FCAL-pattern-for-Emax-larger-than-block-energy-threshold";
-    else if (0 < i && i < 11) cleg = Form("FCAL-pattern-for-Emax-larger-than-%dGeV", i);
-    else if(i == 11) cleg = "FCAL-pattern-for-Emax-larger-than-3GeV-and-with-bad-gains";
-
+    //else if (0 < i && i < 11) cleg = Form("FCAL-pattern-for-Emax-larger-than-%dGeV", i);
+    //else if( i == 11) cleg = "FCAL-pattern-for-Emax-larger-than-3GeV-and-with-bad-gains";
+    else if (i == 1) cleg = "FCAL-pattern-for-Emax-larger-than-3GeV";
+    else if (i == 2) cleg = "FCAL-pattern-for-Emax-larger-than-3GeV-and-with-bad-gains";
+    
     C1 = new TCanvas(cleg, cleg, 10, 10, 1200, 1200);
     gPad->SetLeftMargin(smallBetween1);
     gPad->SetRightMargin(smallBetween2);
@@ -146,9 +149,12 @@ void fcal_monitoring() {
     gPad->SetLogz();
     t->SetTextSize(0.03);
     if (i == 0) t->DrawLatex(-125, 132.5, "Pattern for all E^{cluster}_{max}");
-    if (0 < i &&i < 11) t->DrawLatex(-125, 132.5, Form("Pattern for E^{cluster}_{max} #geq %d GeV", i));
-    else if (i == 11) t->DrawLatex(-125, 132.5, "Pattern for E^{cluster}_{max} #geq4 GeV");
-    if (i == 11) {
+    //if (0 < i &&i < 11) t->DrawLatex(-125, 132.5, Form("Pattern for E^{cluster}_{max} #geq %d GeV", i));
+    //else if (i == 11) t->DrawLatex(-125, 132.5, "Pattern for E^{cluster}_{max} #geq 3 GeV");
+    else if (i == 1) t->DrawLatex(-125, 132.5, "Pattern for E^{cluster}_{max} #geq 3 GeV");
+    else if (i == 2) t->DrawLatex(-125, 132.5, "Pattern for E^{cluster}_{max} #geq 3 GeV");
+    //if (i == 11) {
+    if (i == 2) {
       
       for (int row = 0; row < kBlocksTall; row ++) {
 	
