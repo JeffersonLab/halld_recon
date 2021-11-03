4f5019b0ab0de53961c36a5e79fab4fabdf71221
Simon Taylor <staylor@ifarm1901.jlab.org>
2021-11-03 10:57:23 -0400

    account for attenuation for dE and light propagation in the scintillator for t

--- src/libraries/FMWPC/DCTOFPoint_factory.cc
+++ src/libraries/FMWPC/DCTOFPoint_factory.cc
@@ -30,6 +30,7 @@ jerror_t DCTOFPoint_factory::init(void)
 //------------------
 jerror_t DCTOFPoint_factory::brun(jana::JEventLoop *eventLoop, int32_t runnumber)
 {
+  ATTENUATION_LENGTH=400.;
   LIGHT_PROPAGATION_SPEED=15.; // cm/ns
   THRESHOLD=0.0005; // GeV
 
@@ -44,14 +45,16 @@ jerror_t DCTOFPoint_factory::evnt(JEventLoop *loop, uint64_t eventnumber)
   vector<const DCTOFHit *>ctofhits;
   loop->Get(ctofhits);
   
+  const double paddle_length=120.; // cm
   for (unsigned int i=0;i<ctofhits.size();i++){
     const DCTOFHit *hit1=ctofhits[i];
     for (unsigned int j=i+1;j<ctofhits.size();j++){
       const DCTOFHit *hit2=ctofhits[j];
       if (hit1->bar==hit2->bar && hit1->end!=hit2->end){
-	double dE=sqrt(hit1->dE*hit2->dE);
+	double dE=sqrt(hit1->dE*hit2->dE)
+	  *exp(0.5*paddle_length/ATTENUATION_LENGTH);
 	if (dE>THRESHOLD){
-	  double t=0.5*(hit1->t+hit2->t);
+	  double t=0.5*(hit1->t+hit2->t-paddle_length/LIGHT_PROPAGATION_SPEED);
 	  double y=0.5*LIGHT_PROPAGATION_SPEED*(hit1->t-hit2->t)
 	    *((hit1->end==1)?1.:-1.);
 	  
--- src/libraries/FMWPC/DCTOFPoint_factory.h
+++ src/libraries/FMWPC/DCTOFPoint_factory.h
@@ -24,7 +24,7 @@ class DCTOFPoint_factory:public jana::JFactory<DCTOFPoint>{
   jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
   jerror_t fini(void);						///< Called after last event of last event source has been processed.
 
-  double LIGHT_PROPAGATION_SPEED,THRESHOLD;
+  double LIGHT_PROPAGATION_SPEED,THRESHOLD,ATTENUATION_LENGTH;
 };
 
 #endif // _DCTOFPoint_factory_
