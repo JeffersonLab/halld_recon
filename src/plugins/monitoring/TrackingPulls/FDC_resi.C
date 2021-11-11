d642dd797ac9b66d6c744e3a8798b297d9671590
Keigo MIZUTANI <keigo@jlab.org>
2021-11-10 21:36:28 -0500

    Increase resolution of FDC_resi.png for Plot Browser

--- src/plugins/monitoring/TrackingPulls/FDC_resi.C
+++ src/plugins/monitoring/TrackingPulls/FDC_resi.C
@@ -6,9 +6,11 @@
     hh[i][1] = (TH1D *)gDirectory->Get(Form("TrackingPulls/FDCPulls_Plane%02d/wire_residual_left", i + 1));
     hh[i][0]->SetLineColor(2);
     hh[i][1]->SetLineColor(3);
+    hh[i][0]->SetLineWidth(2);
+    hh[i][1]->SetLineWidth(2);
   }
 
-  TCanvas *c1 = new TCanvas("c1", "c1", 600, 400);
+  TCanvas *c1 = new TCanvas("c1", "c1", 2400, 1600);
   c1->Divide(6, 4);
   TH1 *frame[nplanes];
   TLine *line[nplanes];
