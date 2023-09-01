cf911628e4dee7d688ece572d3549679dcddfeaa
staylorjlab <staylor@ifarm1802.jlab.org>
2023-09-01 16:48:51 -0400

    add meat to GetFieldAndGradient method that useses the DBfieldCartesian_t struct

--- src/libraries/HDGEOMETRY/DMagneticFieldMapSpoiled.h
+++ src/libraries/HDGEOMETRY/DMagneticFieldMapSpoiled.h
@@ -48,7 +48,14 @@ class DMagneticFieldMapSpoiled:public DMagneticFieldMap{
 					 double &dBzdx, double &dBzdy,
 					 double &dBzdz) const;
 		void GetFieldAndGradient(double x,double y,double z,
-					 DBfieldCartesian_t &Bdata) const;
+					 DBfieldCartesian_t &Bdata) const{
+		  GetFieldAndGradient(x,y,z, Bdata.Bx,Bdata.By,Bdata.Bz,
+				       Bdata.dBxdx, Bdata.dBxdy,Bdata.dBxdz,
+				      Bdata.dBydx, Bdata.dBydy,Bdata.dBydz,
+				      Bdata.dBzdx, Bdata.dBzdy,Bdata.dBzdz);
+		  Bdata.Bmag=sqrt(Bdata.Bx*Bdata.Bx+Bdata.By*Bdata.By
+				  +Bdata.Bz*Bdata.Bz);
+		}
 
 	protected:
 		
