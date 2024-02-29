34031b9cb20ed12cc730d2322b6fc6e3fce024ea
Alexander Somov <somov@ifarm1802.jlab.org>
2024-02-29 10:01:35 -0500

    Initial implementation of the ECAL

---
+++ src/libraries/ECAL/DECALDigiHit.h
@@ -0,0 +1,44 @@
+// $Id$
+//
+//    File: DECALDigiHit.h
+
+#ifndef _DECALDigiHit_
+#define _DECALDigiHit_
+
+#include <JANA/JObject.h>
+#include <JANA/JFactory.h>
+
+class DECALDigiHit:public jana::JObject{
+	public:
+		JOBJECT_PUBLIC(DECALDigiHit);
+		
+		int row;
+		int column;
+		uint32_t pulse_integral; ///< identified pulse integral as returned by FPGA algorithm
+		uint32_t pulse_time;     ///< identified pulse time as returned by FPGA algorithm
+		uint32_t pedestal;       ///< pedestal info used by FPGA (if any)
+		uint32_t QF;             ///< Quality Factor from FPGA algorithms
+		uint32_t nsamples_integral;    ///< number of samples used in integral 
+		uint32_t nsamples_pedestal;    ///< number of samples used in pedestal
+		uint32_t pulse_peak;           ///<  maximum sample in pulse
+		
+		uint32_t datasource;           ///<  0=window raw data, 1=old(pre-Fall16) firmware, 2=Df250PulseData
+		
+		// This method is used primarily for pretty printing
+		// the second argument to AddString is printf style format
+		void toStrings(vector<pair<string,string> > &items)const{
+			AddString(items, "row", "%4d", row);
+			AddString(items, "column", "%4d", column);
+			AddString(items, "pulse_integral", "%d", pulse_integral);
+			AddString(items, "pulse_peak", "%d", pulse_peak);
+			AddString(items, "pulse_time", "%d", pulse_time);
+			AddString(items, "pedestal", "%d", pedestal);
+			AddString(items, "QF", "%d", QF);
+			AddString(items, "nsamples_integral", "%d", nsamples_integral);
+			AddString(items, "nsamples_pedestal", "%d", nsamples_pedestal);
+		}
+		
+};
+
+#endif // _DECALDigiHit_
+
---
+++ src/libraries/ECAL/ECAL_init.cc
@@ -0,0 +1,27 @@
+// $Id: FCAL_init.cc 5342 2009-07-09 15:46:12Z davidl $
+
+#include <JANA/JEventLoop.h>
+using namespace jana;
+
+#include <ECAL/DECALTruthShower_factory.h>
+#include <ECAL/DECALGeometry_factory.h>
+
+#include "DECALDigiHit.h"
+#include "DECALHit_factory.h"
+// #include "DECALShower_factory.h"
+
+#include "DECALRefDigiHit.h"
+
+jerror_t ECAL_init(JEventLoop *loop)
+{
+	/// Create and register ECAL data factories
+  	loop->AddFactory(new JFactory<DECALDigiHit>());
+  	loop->AddFactory(new JFactory<DECALRefDigiHit>());
+	loop->AddFactory(new DECALHit_factory());
+	//	loop->AddFactory(new DECALShower_factory());
+	loop->AddFactory(new JFactory<DECALHit>("TRUTH"));
+	//	loop->AddFactory(new DECALTruthShower_factory());
+	loop->AddFactory(new DECALGeometry_factory());
+
+	return NOERROR;
+}
