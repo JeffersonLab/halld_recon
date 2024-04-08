de9f289b1f350dd9d638e3672d9f6e22f4fb0366
raiqarasool <rasoolraiqa@gmail.com>
2024-04-08 10:26:18 -0400

    /src/plugins/Utilities/cal_high_energy_skim ported to JANA2

--- src/plugins/Utilities/cal_high_energy_skim/JEventProcessor_cal_high_energy_skim.h
+++ src/plugins/Utilities/cal_high_energy_skim/JEventProcessor_cal_high_energy_skim.h
@@ -24,12 +24,11 @@ class JEventProcessor_cal_high_energy_skim:public JEventProcessor{
 
 
  private:
-  jerror_t init(void);						///< Called once at program start.
-  jerror_t brun(jana::JEventLoop *eventLoop, int32_t runnumber);	///< Called everytime a new run number is detected.
-  jerror_t evnt(jana::JEventLoop *eventLoop, uint64_t eventnumber);	///< Called every event.
-  jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
-  jerror_t fini(void);	
-  					///< Called after last event of last event source has been processed.
+  void Init() override;
+  void BeginRun(const std::shared_ptr<const JEvent>& event) override;
+  void Process(const std::shared_ptr<const JEvent>& event) override;
+  void EndRun() override;
+  void Finish() override;
 
   double MIN_BCAL_E;
   double MIN_FCAL_E;
