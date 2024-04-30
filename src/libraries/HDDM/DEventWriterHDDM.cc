91734502ff614f19b7bb80b5fe639ffe81774752
Richard Jones <rjones30@gmail.com>
2024-04-30 15:07:30 -0400

    * fix a bug in the DEventWriterHDDM class for saving FWMPC hits [rtj]

--- src/libraries/HDDM/DEventWriterHDDM.cc
+++ src/libraries/HDDM/DEventWriterHDDM.cc
@@ -843,7 +843,8 @@ bool DEventWriterHDDM::Write_HDDMEvent(JEventLoop* locEventLoop, string locOutpu
 	    hddm_s::FmwpcChamberList::iterator FMWPC_ChamberIterator = FMWPC_ChamberList->begin();
 	    for(FMWPC_ChamberIterator = FMWPC_ChamberList->begin(); FMWPC_ChamberIterator != FMWPC_ChamberList->end(); FMWPC_ChamberIterator++)
 	      {
-		if(FMWPCHits[i]->layer == FMWPC_ChamberIterator->getLayer())
+		if(FMWPCHits[i]->layer == FMWPC_ChamberIterator->getLayer() &&
+		   FWMPCHits[i]->wire == FWMPC_ChamberIterator->getWire())
 		  {
 		    foundChamber = true;
 		    break;
