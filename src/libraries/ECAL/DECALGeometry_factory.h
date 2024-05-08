f0a13c0ac8cad0e84057dbce707da029557cb97d
staylorjlab <staylor@ifarm1901.jlab.org>
2024-05-08 17:09:01 -0400

    This factory only needs to do something when the run number changes.

--- src/libraries/ECAL/DECALGeometry_factory.h
+++ src/libraries/ECAL/DECALGeometry_factory.h
@@ -12,48 +12,36 @@
 #include "DECALGeometry.h"
 
 class DECALGeometry_factory:public jana::JFactory<DECALGeometry>{
-	public:
-		DECALGeometry_factory(){};
-		~DECALGeometry_factory(){};
+ public:
+  DECALGeometry_factory(){};
+  ~DECALGeometry_factory(){};
+  
+  DECALGeometry *ecalgeometry = nullptr;
+  
+  //------------------
+  // brun
+  //------------------
+  jerror_t brun(JEventLoop *loop, int32_t runnumber)
+  {
+    DApplication *dapp = dynamic_cast<DApplication*>(loop->GetJApplication());
+    const DGeometry *geom = dapp->GetDGeometry(runnumber);
 
-		DECALGeometry *ecalgeometry = nullptr;
-
-		//------------------
-		// brun
-		//------------------
-		jerror_t brun(JEventLoop *loop, int32_t runnumber)
-		{
-			SetFactoryFlag(NOT_OBJECT_OWNER);
-			ClearFactoryFlag(WRITE_TO_OUTPUT);
-			
-			if( ecalgeometry ) delete ecalgeometry;
-
-			ecalgeometry = new DECALGeometry();
-
-			return NOERROR;
-		}
-
-		//------------------
-		// evnt
-		//------------------
-		 jerror_t evnt(JEventLoop *loop, uint64_t eventnumber)
-		 {
-			// Reuse existing DBCALGeometry object.
-			if( ecalgeometry ) _data.push_back( ecalgeometry );
-			 
-			 return NOERROR;
-		 }
-
-		//------------------
-		// erun
-		//------------------
-		jerror_t erun(void)
-		{
-			if( ecalgeometry ) delete ecalgeometry;
-			ecalgeometry = NULL;
-			
-			return NOERROR;
-		}
+    flags = PERSISTANT;
+    _data.push_back( new DECALGeometry(geom) );
+    
+    return NOERROR;
+  }
+  
+  //------------------
+  // erun
+  //------------------
+  jerror_t erun(void)
+  {
+    for(unsigned int i=0; i<_data.size(); i++)delete _data[i];
+    _data.clear();
+    
+    return NOERROR;
+  }
 };
 
 #endif // _DECALGeometry_factory_
