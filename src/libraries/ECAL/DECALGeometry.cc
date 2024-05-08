50d74279aefbe2a7137e07fe114eaf24db10926b
staylorjlab <staylor@ifarm1901.jlab.org>
2024-05-08 17:06:26 -0400

    Extract crystal positions from xml.

--- src/libraries/ECAL/DECALGeometry.cc
+++ src/libraries/ECAL/DECALGeometry.cc
@@ -17,92 +17,86 @@ using namespace std;
 //---------------------------------
 // DECALGeometry    (Constructor)
 //---------------------------------
-DECALGeometry::DECALGeometry() : 
+DECALGeometry::DECALGeometry(const DGeometry *geom):
   m_numActiveBlocks( 0 ){
-  
-  for( int row = 0; row < kECALBlocksTall; row++ ){
-    for( int col = 0; col < kECALBlocksWide; col++ ){
+
+  // Get positions for each crystal from xml
+  for( int i = 0; i < kECALBlocksTall+2; i++ ){
+    string my_row_string="XTrow"+to_string(i);
+    string my_mpos_string="//composition[@name='"+my_row_string
+      +"']/mposX[@volume='XTModule']/";
+    int ncopy=0;
+    double x0=0,dx=0.;
+    geom->Get(my_mpos_string+"@ncopy",ncopy);
+    geom->Get(my_mpos_string+"@X0",x0);
+    geom->Get(my_mpos_string+"@dX",dx);
+    string my_pos_string="//posXYZ[@volume='"+my_row_string+"']/";
+    vector<double>pos;
+    geom->Get(my_pos_string+"@X_Y_Z",pos);  
+    vector<double>rot;
+    geom->Get(my_pos_string+"@rot",rot);
+    double phi=rot[2]*M_PI/180.;
+    int col0=0,my_row=i;
+    if (i>=40){ //handle right side of beam hole
+      col0=21;
+      my_row=i-21;
+    }
+    for (int col=col0;col<col0+ncopy;col++){
+      double x=x0+double(col-col0)*dx;
+      double y=pos[1]+phi*x; //use small angle approximation
+      m_positionOnFace[my_row][col].Set(x,y);
+      m_row[m_numActiveBlocks]     =  my_row;
+      m_column[m_numActiveBlocks]  =  col;
+      m_activeBlock[my_row][col] = true;
       
-      // transform to beam axis
-      m_positionOnFace[row][col] = 
-	DVector2(  ( (double)col - kECALMidBlock  + 0.5 ) * blockSize(),
-		   ( (double)row - kECALMidBlock + 0.5 ) * blockSize() );
-      if( fabs(m_positionOnFace[row][col].X())>blockSize()
-	  || fabs(m_positionOnFace[row][col].Y())>blockSize()
-	  ){
-	m_activeBlock[row][col] = true;
-	
-	// build the "channel map"
-	m_channelNumber[row][col]    =  m_numActiveBlocks;
-	m_row[m_numActiveBlocks]     =  row;
-	m_column[m_numActiveBlocks]  =  col;
-	
-	m_numActiveBlocks++;
-      }
-      else {
-	m_activeBlock[row][col]=false;
-      }
+      m_numActiveBlocks++;
     }
   }
+  m_activeBlock[19][19]=m_activeBlock[19][20]=false;
+  m_activeBlock[20][19]=m_activeBlock[20][20]=false;
 }
 
 bool
 DECALGeometry::isBlockActive( int row, int column ) const
 {
-	// I'm inserting these lines to effectively disable the
-	// two assert calls below. They are causing all programs
-	// (hd_dump, hdview) to exit, even when I'm not interested
-	// in the FCAL. This does not fix the underlying problem
-	// of why we're getting invalid row/column values.
-	// 12/13/05  DL
-	if( row < 0 ||  row >= kECALBlocksTall )return false;
-	if( column < 0 ||  column >= kECALBlocksWide )return false;
-
-	assert(    row >= 0 &&    row < kECALBlocksTall );
-	assert( column >= 0 && column < kECALBlocksWide );
+  if( row < 0 ||  row >= kECALBlocksTall )return false;
+  if( column < 0 ||  column >= kECALBlocksWide )return false;
 	
-	return m_activeBlock[row][column];	
+  return m_activeBlock[row][column];	
 }
 
 int
 DECALGeometry::row( float y ) const 
 {	
-	return static_cast<int>( y / blockSize() + kECALMidBlock );
+  return static_cast<int>( y / blockSize() + kECALMidBlock );
 }
 
 int
 DECALGeometry::column( float x ) const 
 {	
-	return static_cast<int>( x / blockSize() + kECALMidBlock );
+  return static_cast<int>( x / blockSize() + kECALMidBlock );
 }
 
 DVector2
 DECALGeometry::positionOnFace( int row, int column ) const
 { 
-	assert(    row >= 0 &&    row < kECALBlocksTall );
-	assert( column >= 0 && column < kECALBlocksWide );
-	
-	return m_positionOnFace[row][column]; 
+  return m_positionOnFace[row][column]; 
 }
 
 DVector2
 DECALGeometry::positionOnFace( int channel ) const
 {
-	assert( channel >= 0 && channel < m_numActiveBlocks );
-	
-	return positionOnFace( m_row[channel], m_column[channel] );
+  return positionOnFace( m_row[channel], m_column[channel] );
 }
 
 int
 DECALGeometry::channel( int row, int column ) const
 {
-	if( isBlockActive( row, column ) ){
-		
-		return m_channelNumber[row][column]; 
-	}
-	else{
-		
-		cerr << "ERROR: request for channel number of inactive block!" << endl;
-		return -1;
-	}
+  if( isBlockActive( row, column ) ){
+    return m_channelNumber[row][column]; 
+  }
+  else{    
+    cerr << "ERROR: request for channel number of inactive block!" << endl;
+    return -1;
+  }
 }
