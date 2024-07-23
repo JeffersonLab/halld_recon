5dd6345e3c79bbc90ea7502307af885f142ca56c
staylorjlab <staylor@ifarm2401.jlab.org>
2024-07-23 08:03:25 -0400

    Add status word

--- src/libraries/FCAL/DFCALCluster.h
+++ src/libraries/FCAL/DFCALCluster.h
@@ -24,108 +24,111 @@ using namespace jana;
 #define MAX_SHOWER_RADIUS 25
 
 class DFCALCluster : public JObject {
-   public:
-      JOBJECT_PUBLIC(DFCALCluster);
-                
-      DFCALCluster( const int nhits );
-      ~DFCALCluster();
-
-   typedef struct {
-     oid_t id;
-     int ch;
-     float x;
-     float y;
-     float E;
-     float t;
+public:
+  JOBJECT_PUBLIC(DFCALCluster);
+  
+  DFCALCluster( const int nhits );
+  ~DFCALCluster();
+  
+  typedef struct {
+    oid_t id;
+    int ch;
+    float x;
+    float y;
+    float E;
+    float t;
    } userhit_t;
+  
+  class DFCALClusterHit_t{
+  public:
+    DFCALClusterHit_t(int ch,double E,double x,double y,double t)
+      :ch(ch),E(E),x(x),y(y),t(t){}
+    int ch;
+    double E;
+    double x;
+    double y;
+    double t;
+  };
+  
+  void saveHits( vector<userhit_t>& hit );
 
-   class DFCALClusterHit_t{
-   public:
-   DFCALClusterHit_t(int ch,double E,double x,double y,double t)
-     :ch(ch),E(E),x(x),y(y),t(t){}
-     int ch;
-     double E;
-     double x;
-     double y;
-     double t;
-   };
-   
-   void saveHits( vector<userhit_t>& hit );
+  void setTimeEWeight(double myTimeEWeight){fTimeEWeight=myTimeEWeight;};
+  void setEnergy(double myEnergy){fEnergy=myEnergy;};
+  void setChannelEmax(int myChannelEmax){fChannelEmax=myChannelEmax;};
+  void setCentroid(double myX,double myY){fCentroid.SetXYZ(myX,myY,0.);};
+  void setStatus(int myStatus) {fStatus=myStatus;};
+  
+  double getEexpected(const int ihit) const;
+  double getEallowed(const int ihit) const;
+  double getEnergy() const;
+  double getEmax() const;
+  int getChannelEmax() const;
+  double getTime() const;
+  double getTimeMaxE() const;
+  double getTimeEWeight() const;
+  // the centroid returned by this function is in 
+  // a frame where z=0 is at the center of the target
+  DVector3 getCentroid() const;
+  
+  double getRMS() const;
+  double getRMS_t() const;
+  double getRMS_x() const;
+  double getRMS_y() const;
+  double getRMS_u() const;
+  double getRMS_v() const;
 
-   void setTimeEWeight(double myTimeEWeight){fTimeEWeight=myTimeEWeight;};
-   void setEnergy(double myEnergy){fEnergy=myEnergy;};
-   void setChannelEmax(int myChannelEmax){fChannelEmax=myChannelEmax;};
-   void setCentroid(double myX,double myY){fCentroid.SetXYZ(myX,myY,0.);};
+  int getStatus() const {return fStatus;};
+  
+  int getHits() const; // get number of hits owned by a cluster
+  int addHit(const int ihit, const double frac);
+  void resetClusterHits();
+  bool update( vector<userhit_t>& hitList, double fcalFaceZ,
+	       const DFCALGeometry *fcalgeom );
+  void addHit(int ch,double E,double x,double y,double t);
+  
+  // get hits that form a cluster after clustering is finished
+  inline const vector<DFCALClusterHit_t> GetHits() const { return my_hits; }
+  inline uint32_t GetNHits(void) const { return my_hits.size(); }
+  
+  void toStrings(vector<pair<string,string> > &items) const {
+    AddString(items, "x(cm)", "%3.1f", getCentroid().x());
+    AddString(items, "y(cm)", "%3.1f", getCentroid().y());
+    AddString(items, "E(GeV)", "%2.3f", getEnergy());
+    AddString(items, "t(ns)", "%2.3f", getTimeEWeight());
+  }
 
-   double getEexpected(const int ihit) const;
-   double getEallowed(const int ihit) const;
-   double getEnergy() const;
-   double getEmax() const;
-    int getChannelEmax() const;
-   double getTime() const;
-   double getTimeMaxE() const;
-   double getTimeEWeight() const;
-   // the centroid returned by this function is in 
-   // a frame where z=0 is at the center of the target
-   DVector3 getCentroid() const;
+private:
 
-   double getRMS() const;
-   double getRMS_t() const;
-   double getRMS_x() const;
-   double getRMS_y() const;
-   double getRMS_u() const;
-   double getRMS_v() const;
-
-   int getHits() const; // get number of hits owned by a cluster
-   int addHit(const int ihit, const double frac);
-   void resetClusterHits();
-   bool update( vector<userhit_t>& hitList, double fcalFaceZ,
-		const DFCALGeometry *fcalgeom );
-   void addHit(int ch,double E,double x,double y,double t);
-
-   // get hits that form a cluster after clustering is finished
-   inline const vector<DFCALClusterHit_t> GetHits() const { return my_hits; }
-   inline uint32_t GetNHits(void) const { return my_hits.size(); }
-
-   void toStrings(vector<pair<string,string> > &items) const {
-      AddString(items, "x(cm)", "%3.1f", getCentroid().x());
-      AddString(items, "y(cm)", "%3.1f", getCentroid().y());
-      AddString(items, "E(GeV)", "%2.3f", getEnergy());
-      AddString(items, "t(ns)", "%2.3f", getTimeEWeight());
-   }
-
-   private:
-
-   void shower_profile( vector<userhit_t>& hitList, 
-                        const int ihit,
-                        double& Eallowed, double& Eexpected, 
-			double fcalMidplaneZ, 
-			const DFCALGeometry *fcalgeom ) const ;
-
-   double fEnergy;        // total cluster energy (GeV) or 0 if stale
-   double fTime;          // cluster time(ns) set equivalent to fTimeMaxE below
-   double fTimeMaxE;      // cluster time(ns) set by first (max E) block, for now
-   double fTimeEWeight;   // cluster time(ns) energy weighted average
-   double fEmax;          // energy in the first block of the cluster
-    int fChannelEmax;      // Channel number with maximum energy
-   DVector3 fCentroid;    // cluster centroid position (cm) [z=0 at target center]
-   double fRMS;           // cluster r.m.s. size (cm)
-   double fRMS_t;         // r.m.s. of energy weighted hits (ns)
-   double fRMS_x;         // cluster r.m.s. size along X-axis (cm)
-   double fRMS_y;         // cluster r.m.s. size along Y-axis (cm)
-   double fRMS_u;         // cluster r.m.s. size in radial direction (cm)
-   double fRMS_v;         // cluster r.m.s. size in azimuth direction (cm)
-   int m_nFcalHits;       // total number of hits to work with
-                          //   need to rename other member data  
-   int fNhits;            // number of hits owned by this cluster
-   int *fHit;             // index list of hits owned by this cluster
-   double *fHitf;         // list of hit fractions owned by this cluster
-   double *fEexpected;    // expected energy of hit by cluster (GeV)
-   double *fEallowed;     // allowed energy of hit by cluster (GeV)
-
-   // container for hits that form a cluster to be used after clustering is done
-   vector<DFCALClusterHit_t> my_hits; 
-
+  void shower_profile( vector<userhit_t>& hitList, 
+		       const int ihit,
+		       double& Eallowed, double& Eexpected, 
+		       double fcalMidplaneZ, 
+		       const DFCALGeometry *fcalgeom ) const ;
+  
+  double fEnergy;        // total cluster energy (GeV) or 0 if stale
+  double fTime;          // cluster time(ns) set equivalent to fTimeMaxE below
+  double fTimeMaxE;      // cluster time(ns) set by first (max E) block, for now
+  double fTimeEWeight;   // cluster time(ns) energy weighted average
+  double fEmax;          // energy in the first block of the cluster
+  int fChannelEmax;      // Channel number with maximum energy
+  DVector3 fCentroid;    // cluster centroid position (cm) [z=0 at target center]
+  double fRMS;           // cluster r.m.s. size (cm)
+  double fRMS_t;         // r.m.s. of energy weighted hits (ns)
+  double fRMS_x;         // cluster r.m.s. size along X-axis (cm)
+  double fRMS_y;         // cluster r.m.s. size along Y-axis (cm)
+  double fRMS_u;         // cluster r.m.s. size in radial direction (cm)
+  double fRMS_v;         // cluster r.m.s. size in azimuth direction (cm)
+  int m_nFcalHits;       // total number of hits to work with
+  //   need to rename other member data  
+  int fNhits;            // number of hits owned by this cluster
+  int *fHit;             // index list of hits owned by this cluster
+  double *fHitf;         // list of hit fractions owned by this cluster
+  double *fEexpected;    // expected energy of hit by cluster (GeV)
+  double *fEallowed;     // allowed energy of hit by cluster (GeV)
+  int fStatus=0; // Status word: 0 = main peak found; 1 = second peak found; 2 = split found    
+  
+  // container for hits that form a cluster to be used after clustering is done
+  vector<DFCALClusterHit_t> my_hits; 
 };
 
 inline double DFCALCluster::getEexpected(const int ihit) const
