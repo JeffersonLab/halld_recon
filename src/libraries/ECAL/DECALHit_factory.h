178f9ee3a0d4d28199b2c298736a066234ca2af5
staylorjlab <staylor@ifarm2401.jlab.org>
2024-08-22 08:06:34 -0400

    Remove DECALGeometry and move its functionality to the FCAL library where it is needed.

--- src/libraries/ECAL/DECALHit_factory.h
+++ src/libraries/ECAL/DECALHit_factory.h
@@ -20,35 +20,50 @@ using namespace std;
 typedef  vector< vector<double> >  ecal_constants_t;
 
 class DECALHit_factory:public jana::JFactory<DECALHit>{
-	public:
-                DECALHit_factory();
-		~DECALHit_factory(){};
+public:
+  DECALHit_factory();
+  ~DECALHit_factory(){};
+  
+  ecal_constants_t  gains;
+  ecal_constants_t  pedestals;		
+  ecal_constants_t  time_offsets;
+  ecal_constants_t  adc_offsets;
+  
+  double adc_en_scale;
+  double adc_time_scale;
+  
+  double base_time;
 
-		ecal_constants_t  gains;
-		ecal_constants_t  pedestals;		
-		ecal_constants_t  time_offsets;
-		ecal_constants_t  adc_offsets;
+  bool isBlockActive( int row, int column ) const {
+    if( row < 0 ||  row >= kECALBlocksTall )return false;
+    if( column < 0 ||  column >= kECALBlocksWide )return false;
+    
+    return m_activeBlock[row][column];    
+  }
 
-		double adc_en_scale;
-		double adc_time_scale;
-		
-		double base_time;
+private:
+  jerror_t init(void);						///< Called once at program start.2
+  jerror_t brun(jana::JEventLoop *eventLoop, int32_t runnumber);	///< Called everytime a new run number is detected.
+  jerror_t evnt(jana::JEventLoop *eventLoop, uint64_t eventnumber);	///< Called every event.
+  jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
+  jerror_t fini(void);						///< Called after last event of last event source has been processed.
+  
+  void LoadECALConst(ecal_constants_t &table,
+		     const vector<double> &ecal_const_ch);    
 
-        private:
-		jerror_t init(void);						///< Called once at program start.2
-		jerror_t brun(jana::JEventLoop *eventLoop, int32_t runnumber);	///< Called everytime a new run number is detected.
-		jerror_t evnt(jana::JEventLoop *eventLoop, uint64_t eventnumber);	///< Called every event.
-		jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
-		jerror_t fini(void);						///< Called after last event of last event source has been processed.
+  static const int kECALBlocksWide   = 40;
+  static const int kECALBlocksTall   = 40;
+  static const int kECALMidBlock     = 20;
+  static const int kECALMaxChannels  = kECALBlocksWide * kECALBlocksTall;
 
-		void LoadECALConst( ecal_constants_t &table, 
-                                    const vector<double> &ecal_const_ch, 
-                                    const DECALGeometry  &ecalGeom);    
+  unsigned int DB_PEDESTAL;
+  unsigned int HIT_DEBUG;
 
-
-		unsigned int DB_PEDESTAL;
-		unsigned int HIT_DEBUG;
-
+  bool   m_activeBlock[kECALBlocksTall][kECALBlocksWide];
+  int    m_channelNumber[kECALBlocksTall][kECALBlocksWide];
+  int    m_row[kECALMaxChannels];
+  int    m_column[kECALMaxChannels];
+  int    m_numActiveBlocks;
 };
 
 #endif // _DECALHit_factory_
