d5bc32d543113aad531337b5af2d352a1987e4f9
staylorjlab <staylor@ifarm2401.jlab.org>
2025-03-27 14:07:07 -0400

    Get insert geometry from xml

--- src/libraries/ECAL/DECALGeometry.h
+++ src/libraries/ECAL/DECALGeometry.h
@@ -13,60 +13,58 @@
 
 #include "DVector2.h"
 #include "units.h"
+#include <HDGEOMETRY/DGeometry.h>
 
 class DECALGeometry:public JObject{
+public:
+  JOBJECT_PUBLIC(DECALGeometry);
+  
+  DECALGeometry(const DGeometry *geom);
+  ~DECALGeometry(){}
+  
+  static const int kECALBlocksWide   = 40;
+  static const int kECALBlocksTall   = 40;
+  static const int kECALMaxChannels  = kECALBlocksWide * kECALBlocksTall;
+  static const int kECALBeamHoleSize = 2;
 
-	public:
-		JOBJECT_PUBLIC(DECALGeometry);
-		
-		DECALGeometry();
-		~DECALGeometry(){}
-
-		static const int kECALBlocksWide   = 40;
-		static const int kECALBlocksTall   = 40;
-		static const int kECALMaxChannels  = kECALBlocksWide * kECALBlocksTall;
-		static const int kECALMidBlock     = (kECALBlocksWide)/2;
-		static const int kECALBeamHoleSize = 2;
-
-		static double blockSize()  { return  2.09 * k_cm; }
-		static double blockLength(){ return  20.0 * k_cm; }
-		static double ecalFaceZ()  { return  1279.376 * k_cm; }
+  static double blockSize()  { return  2.09 * k_cm; }
+  static double blockLength(){ return  20.0 * k_cm; }
+  
+  int numActiveBlocks() const { return m_numActiveBlocks; }
+  DVector2 positionOnFace(int row,int column) const {return m_positionOnFace[row][column];}
+  DVector2 positionOnFace( int channel ) const {return positionOnFace( m_row[channel], m_column[channel]);} 
+  double sensitiveBlockSize() const {return m_sensitiveBlockSize;}
+  double insertFrontZ() const {return m_insertFrontZ;}
+  
+  int channel( int row, int column ) const {return m_channelNumber[row][column];} 
+  int row   ( int channel ) const { return m_row[channel];    }
+  int column( int channel ) const { return m_column[channel]; }
 	
-		static double ecalMidplane() { return ecalFaceZ() + 0.5 * blockLength() ; } 
-	
-		bool isBlockActive( int row, int column ) const;
-		int  numActiveBlocks() const { return m_numActiveBlocks; }
+  // get row and column from x and y positions
+  int row   ( double y ) const;
+  int column( double x ) const;
+  // get channel number from x and y
+  int channel(double x,double y) const;
+  
+  void Summarize(JObjectSummary& summary) const{
+    summary.add((int) kECALBlocksWide, "kECALBlocksWide", "%d");
+    summary.add((int) kECALBlocksTall, "kECALBlocksTall", "%d");
+    summary.add((int) kECALMaxChannels, "kECALMaxChannels", "%d");
+    summary.add((int) kECALBeamHoleSize, "kECALBeamHoleSize", "%2.3f");
+  }
+  
+private:
+  DVector2 m_positionOnFace[kECALBlocksTall][kECALBlocksWide];
+  double m_insertFrontZ;
+  double m_sensitiveBlockSize;
+  
+  int    m_channelNumber[kECALBlocksTall][kECALBlocksWide];
+  int    m_row[kECALMaxChannels];
+  int    m_column[kECALMaxChannels];
+  
+  int    m_numActiveBlocks;
 
-	
-		DVector2 positionOnFace( int row, int column ) const;
-		DVector2 positionOnFace( int channel ) const;
-	
-		int channel( int row, int column ) const;
-
-		int row   ( int channel ) const { return m_row[channel];    }
-		int column( int channel ) const { return m_column[channel]; }
-	
-		// get row and column from x and y positions
-		int row   ( float y ) const;
-		int column( float x ) const;
-
-		void Summarize(JObjectSummary& summary) const{
-		  summary.add((int) kECALBlocksWide, "kECALBlocksWide", "%d");
-		  summary.add((int) kECALBlocksTall, "kECALBlocksTall", "%d");
-		  summary.add((int) kECALMaxChannels, "kECALMaxChannels", "%d");
-		  summary.add((int) kECALBeamHoleSize, "kECALBeamHoleSize", "%2.3f");
-		}
-	
-	private:
-
-		bool   m_activeBlock[kECALBlocksTall][kECALBlocksWide];
-		DVector2 m_positionOnFace[kECALBlocksTall][kECALBlocksWide];
-
-		int    m_channelNumber[kECALBlocksTall][kECALBlocksWide];
-		int    m_row[kECALMaxChannels];
-		int    m_column[kECALMaxChannels];
-	
-		int    m_numActiveBlocks;
+  DECALGeometry(){};// force use of constructor with arguments.
 };
 
 #endif // _DECALGeometry_
