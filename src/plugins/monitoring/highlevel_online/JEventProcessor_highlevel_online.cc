8b5f4564f3c54d3c9d79cff2e9d300b44c561236
jrstevenjlab <jrsteven@jlab.org>
2025-04-29 13:17:30 -0400

    add ecal to ADC sums for trigger

--- src/plugins/monitoring/highlevel_online/JEventProcessor_highlevel_online.cc
+++ src/plugins/monitoring/highlevel_online/JEventProcessor_highlevel_online.cc
@@ -224,9 +224,9 @@ void JEventProcessor_highlevel_online::Init()
 	dHist_NumTriggers->GetXaxis()->SetBinLabel(33, "Total");
 
 
+	dHist_BCALVsFCAL2_TrigBit1 = new TH2I("BCALVsFCAL2_TrigBit1","TRIG BIT 1;E (FCAL2) (count);E (BCAL) (count)", 200, 0., 10000, 200, 0., 50000);
 	dHist_BCALVsFCAL_TrigBit1 = new TH2I("BCALVsFCAL_TrigBit1","TRIG BIT 1;E (FCAL) (count);E (BCAL) (count)", 200, 0., 10000, 200, 0., 50000);
-
-	dHist_CCALVsFCAL_TrigBit1 = new TH2I("CCALVsFCAL_TrigBit1","TRIG BIT 1;E (FCAL) (count);E (CCAL) (count)", 100, 0., 20000, 200, 0., 35000);
+	dHist_ECALVsFCAL_TrigBit1 = new TH2I("ECALVsFCAL_TrigBit1","TRIG BIT 1;E (FCAL) (count);E (ECAL) (count)", 200, 0., 20000, 200, 0., 20000);
 
 	dHist_L1bits_gtp = new TH1I("L1bits_gtp", "L1 trig bits from GTP;Trig. bit (1-32)", 34, 0.5, 34.5);
 	dHist_L1bits_fp  = new TH1I("L1bits_fp", "L1 trig bits from FP;Trig. bit (1-32)", 32, 0.5, 32.5);
@@ -370,12 +370,20 @@ void JEventProcessor_highlevel_online::BeginRun(const std::shared_ptr<const JEve
 
 	fcal_cell_thr  =  65;
 	bcal_cell_thr  =  20;
-	ccal_cell_thr  =  30;
+	ecal_cell_thr  =  35;
 
-	fcal_row_mask_min = 26;
-	fcal_row_mask_max = 32;
-	fcal_col_mask_min = 26;
-	fcal_col_mask_max = 32;
+	ecal_row_mask_min = 17;
+	ecal_row_mask_max = 22;
+	ecal_col_mask_min = 17;
+	ecal_col_mask_max = 22;
+	
+	if( event->GetRunNumber() < 1300000 )
+	{
+		fcal_row_mask_min = 26;
+		fcal_row_mask_max = 32;
+		fcal_col_mask_min = 26;
+		fcal_col_mask_max = 32;
+	}
 
 	if( event->GetRunNumber() < 11127 )
 	{
@@ -427,8 +435,8 @@ void JEventProcessor_highlevel_online::Process(const std::shared_ptr<const JEven
 	vector<const DBCALDigiHit*> locBCALDigiHits;
 	locEvent->Get(locBCALDigiHits);
         
-	vector<const DCCALDigiHit*> locCCALDigiHits;
-	locEvent->Get(locCCALDigiHits);
+	vector<const DECALDigiHit*> locECALDigiHits;
+	locEvent->Get(locECALDigiHits);
 
         // BCAL LED Pseudo Trigger//
         vector<const DBCALHit*> locdbcalhits;
@@ -592,7 +600,7 @@ void JEventProcessor_highlevel_online::Process(const std::shared_ptr<const JEven
 		}
 	}
 
-	//Get total FCAL energy
+	//Get total FCAL energy (10 - 25
 	int fcal_tot_en = 0;
 	for( auto fcal_hit : locFCALDigiHits){
 
@@ -606,14 +614,14 @@ void JEventProcessor_highlevel_online::Process(const std::shared_ptr<const JEven
 	  if( ((int32_t)fcal_hit->pulse_peak-100) <= fcal_cell_thr) continue;
 	  
 	  uint32_t adc_time = (fcal_hit->pulse_time >> 6) & 0x1FF; // consider only course time
-	  if((adc_time < 15) || (adc_time > 50)) continue; // changed from 20 and 70 based on run 30284  2/5/2017 DL
+	  //if((adc_time < 15) || (adc_time > 50)) continue; // changed from 20 and 70 based on run 30284  2/5/2017 DL
 	  
 	  Int_t pulse_int = fcal_hit->pulse_integral - fcal_hit->nsamples_integral*100;
 	  if(pulse_int < 0) continue;
 	  fcal_tot_en += pulse_int;
 	}
 	
-	//Get total BCAL energy
+	//Get total BCAL energy 
 	int bcal_tot_en = 0;
 	for(auto bcal_hit : locBCALDigiHits){
 		if( ((int32_t)bcal_hit->pulse_peak-100) <= bcal_cell_thr) continue;
@@ -622,13 +630,20 @@ void JEventProcessor_highlevel_online::Process(const std::shared_ptr<const JEven
 		bcal_tot_en += pulse_int;
 	}
 
-	//Get total CCAL energy
-	int ccal_tot_en = 0;
-	for(auto ccal_hit : locCCALDigiHits){
-	  if( ((int32_t)ccal_hit->pulse_peak-100) <= ccal_cell_thr) continue;
-	  Int_t pulse_int = ccal_hit->pulse_integral - ccal_hit->nsamples_integral*100;
-	  if(pulse_int < 0) continue;
-	  ccal_tot_en += pulse_int;
+	//Get total ECAL energy 15 - 40
+	int ecal_tot_en = 0;
+	for(auto ecal_hit : locECALDigiHits){
+		int row = ecal_hit->row;
+		int col = ecal_hit->column;
+		
+		if( (row >= ecal_row_mask_min) && (row <= ecal_row_mask_max) ){
+			if( (col >= ecal_col_mask_min) && (col <= ecal_col_mask_max) ) continue;
+		}
+		
+		if( ((int32_t)ecal_hit->pulse_peak-100) <= ecal_cell_thr) continue;
+		Int_t pulse_int = ecal_hit->pulse_integral - ecal_hit->nsamples_integral*100;
+		if(pulse_int < 0) continue;
+		ecal_tot_en += pulse_int;
 	}
 
 	
@@ -759,8 +774,9 @@ void JEventProcessor_highlevel_online::Process(const std::shared_ptr<const JEven
 	  
 		if(locgtpTrigBits[0] == 1) //bit 1
                   {
-                    dHist_BCALVsFCAL_TrigBit1->Fill(Float_t(fcal_tot_en), Float_t(bcal_tot_en));
-                    dHist_CCALVsFCAL_TrigBit1->Fill(Float_t(fcal_tot_en), Float_t(ccal_tot_en));
+		    dHist_BCALVsFCAL2_TrigBit1->Fill(Float_t(fcal_tot_en)+Float_t(ecal_tot_en), Float_t(bcal_tot_en));
+		    dHist_BCALVsFCAL_TrigBit1->Fill(Float_t(fcal_tot_en), Float_t(bcal_tot_en));
+                    dHist_ECALVsFCAL_TrigBit1->Fill(Float_t(fcal_tot_en), Float_t(ecal_tot_en));
                   }
 
 		// trigger bits
