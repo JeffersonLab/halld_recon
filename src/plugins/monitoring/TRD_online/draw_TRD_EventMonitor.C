0d576416246cd0caecdcc9b604f14384e726574f
Nizar Septian <nizar47@gmail.com>
2025-05-30 18:44:15 -0400

    Add macro to draw DigiHits, Hits, Clusters, Point, Segments, Tracks of an event

---
+++ src/plugins/monitoring/TRD_online/draw_TRD_EventMonitor.C
@@ -0,0 +1,215 @@
+
+const Int_t NTRDplanes = 2;
+const Int_t NEventsMonitor = 100;
+const Int_t NMaxExtrapolations = 5;
+const Int_t NMaxSegments = 5;
+
+struct Segment {
+    double x;
+    double y;
+    double tx;
+    double ty;
+    double var_x;
+    double var_y;
+    double var_tx;
+    double var_ty;
+    TF1 *segment_path_x;
+    TF1 *segment_path_y;
+    Segment(double x, double y, double tx, double ty, double var_x, double var_y, double var_tx, double var_ty) :
+        x(x), y(y), tx(tx), ty(ty), var_x(var_x), var_y(var_y), var_tx(var_tx), var_ty(var_ty) {
+            segment_path_x = new TF1("segment_path_x", "[0]*(x-532.4)+[1]", 528, 533);
+            segment_path_x->SetParameter(0, tx);
+            segment_path_x->SetParameter(1, x);
+            segment_path_y = new TF1("segment_path_y", "[0]*(x-532.4)+[1]", 528, 533);
+            segment_path_y->SetParameter(0, ty);
+            segment_path_y->SetParameter(1, y);
+        };
+};
+
+struct Extrapolation {
+    double x;
+    double y;
+    double dxdz;
+    double dydz;
+    TF1 *extrapolation_path_x;
+    TF1 *extrapolation_path_y;
+    Extrapolation(double x, double y, double dxdz, double dydz) :
+        x(x), y(y), dxdz(dxdz), dydz(dydz) {
+            extrapolation_path_x = new TF1("extrapolation_path_x", "[0]*(x-532.4)+[1]", 528, 533);
+            extrapolation_path_x->SetParameter(0, dxdz);
+            extrapolation_path_x->SetParameter(1, x);
+            extrapolation_path_y = new TF1("extrapolation_path_y", "[0]*(x-532.4)+[1]", 528, 533);
+            extrapolation_path_y->SetParameter(0, dydz);
+            extrapolation_path_y->SetParameter(1, y);
+        };
+};
+
+
+void draw_TRD_EventMonitor(TString root_file = "/work/halld2/home/nseptian/TRD-DEV/hd_root_131346_TRDOnline.root") {
+    
+    TFile *f = TFile::Open(root_file);
+    TDirectory *dir = (TDirectory*)f->Get("TRD");
+    TDirectory *subdir = (TDirectory*)dir->Get("EventMonitor");
+
+    TH2I *hClusterHits_TimeVsPos[NTRDplanes][NEventsMonitor];
+    TH2I *hCluster_TimeVsPos[NTRDplanes][NEventsMonitor];
+	TH2I *hDigiHit_TimeVsStrip[NTRDplanes][NEventsMonitor];
+    TH2I *hPoint_TimeVsPos[NTRDplanes][NEventsMonitor];
+    TH2I *hPoint_XvsZ[NEventsMonitor];
+    TH2I *hPoint_YvsZ[NEventsMonitor];
+    TH2D *hSegment_Members[NEventsMonitor][NMaxSegments];
+    TH2D *hExtrapolation_Members[NEventsMonitor][NMaxExtrapolations];
+
+    vector<vector<Segment*>> v_segments;
+    vector<vector<Extrapolation*>> v_extrapolations;
+
+    for (unsigned int i = 0; i < NEventsMonitor; i++) {
+        vector<Segment*> segments;
+        vector<Extrapolation*> extrapolations;
+
+        hPoint_XvsZ[i] = (TH2I*)subdir->Get(Form("Point_ZVsX_Event%d", i));
+        hPoint_YvsZ[i] = (TH2I*)subdir->Get(Form("Point_ZVsY_Event%d", i));
+        
+        for (unsigned int j = 0; j < NMaxSegments; j++) {
+            hSegment_Members[i][j] = (TH2D*)subdir->Get(Form("Segment_Members_Event%d_Segment%d", i, j));
+            
+            Segment *segment = new Segment(
+                hSegment_Members[i][j]->GetBinContent(1),
+                hSegment_Members[i][j]->GetBinContent(2),
+                hSegment_Members[i][j]->GetBinContent(3),
+                hSegment_Members[i][j]->GetBinContent(4),
+                hSegment_Members[i][j]->GetBinContent(5),
+                hSegment_Members[i][j]->GetBinContent(6),
+                hSegment_Members[i][j]->GetBinContent(7),
+                hSegment_Members[i][j]->GetBinContent(8)
+            );
+            
+            if (segment->x ==0. || segment->y == 0. || isnan(segment->x) || isnan(segment->y)) {
+                // cout << "Segment x = " << segment->x << ", y = " << segment->y << ", tx = " << segment->tx << ", ty = " << segment->ty << endl;
+                delete segment;
+                continue;
+            } else {
+                // cout << "Segment x = " << segment->x << ", y = " << segment->y << ", tx = " << segment->tx << ", ty = " << segment->ty << endl;
+                segments.push_back(segment);
+                // cout << segments.size() << " segments for event " << i << endl;
+            }    
+        }
+        v_segments.push_back(segments);
+
+        for (unsigned int j = 0; j < NMaxExtrapolations; j++) {
+            hExtrapolation_Members[i][j] = (TH2D*)subdir->Get(Form("Extrapolation_Members_Event%d_Extrapolation%d", i, j));
+            
+            Extrapolation *extrapolation = new Extrapolation(
+                hExtrapolation_Members[i][j]->GetBinContent(1),
+                hExtrapolation_Members[i][j]->GetBinContent(2),
+                hExtrapolation_Members[i][j]->GetBinContent(3),
+                hExtrapolation_Members[i][j]->GetBinContent(4)
+            );
+            if (extrapolation->x ==0. || extrapolation->y == 0.) {
+                // cout << "Extrapolation x = " << extrapolation->x << ", y = " << extrapolation->y << ", dxdz = " << extrapolation->dxdz << ", dydz = " << extrapolation->dydz << endl;            
+                // cout << extrapolations.size() << " extrapolations for event " << i << endl;
+                delete extrapolation;
+                continue;
+            } else {
+                // cout << "Extrapolation x = " << extrapolation->x << ", y = " << extrapolation->y << ", dxdz = " << extrapolation->dxdz << ", dydz = " << extrapolation->dydz << endl;
+                extrapolations.push_back(extrapolation);
+            }
+        }
+        v_extrapolations.push_back(extrapolations);   
+    }
+	
+    for (int i = 0; i < NTRDplanes; i++) {
+        for (int j = 0; j < NEventsMonitor; j++) {
+            hClusterHits_TimeVsPos[i][j] = (TH2I*)subdir->Get(Form("ClusterHits_TimeVsPos_Plane%d_Event%d", i, j));
+            hCluster_TimeVsPos[i][j] = (TH2I*)subdir->Get(Form("Cluster_TimeVsPos_Plane%d_Event%d", i, j));
+			hDigiHit_TimeVsStrip[i][j] = (TH2I*)subdir->Get(Form("DigiHit_TimeVsStrip_Plane%d_Event%d", i, j));
+            hPoint_TimeVsPos[i][j] = (TH2I*)subdir->Get(Form("Point_TimeVsPos_Plane%d_Event%d", i, j));
+        }
+    }
+
+    TCanvas *c = new TCanvas("c", "c", 2000, 800);
+    c->Divide(4, 2);
+
+    TString pdfFileName = "hd_root_EventMonitor_output_";
+    pdfFileName += NEventsMonitor;
+    pdfFileName += "events.pdf";
+    
+    c->Print(pdfFileName + "[");
+
+    gStyle->SetOptStat(0);
+	gStyle->SetPalette(kRainBow);
+    for (int i = 0; i < NEventsMonitor; i++) {
+        for (int j = 0; j < NTRDplanes; j++) {
+
+			c->cd(4*j+1);
+            hDigiHit_TimeVsStrip[j][i]->Draw("COLZ");
+            hDigiHit_TimeVsStrip[j][i]->GetXaxis()->SetTitle("Time (ns)");
+            hDigiHit_TimeVsStrip[j][i]->GetYaxis()->SetTitle("Strip");
+
+            c->cd(4*j+2);
+            hClusterHits_TimeVsPos[j][i]->Draw("COLZ");
+            hClusterHits_TimeVsPos[j][i]->GetXaxis()->SetTitle("Time (ns)");
+            hClusterHits_TimeVsPos[j][i]->GetYaxis()->SetTitle("Position (cm)");
+            
+            hCluster_TimeVsPos[j][i]->Draw("SAME");
+            hCluster_TimeVsPos[j][i]->SetMarkerStyle(kCircle);
+            hCluster_TimeVsPos[j][i]->SetMarkerSize(2.0);
+
+            c->cd(4*j+3);
+            hPoint_TimeVsPos[j][i]->Draw("P");
+            hPoint_TimeVsPos[j][i]->SetMarkerStyle(kFullCircle);
+            hPoint_TimeVsPos[j][i]->SetMarkerSize(1.0);
+            hPoint_TimeVsPos[j][i]->GetXaxis()->SetTitle("Time (ns)");
+            hPoint_TimeVsPos[j][i]->GetYaxis()->SetTitle("Position (cm)");
+
+            c->cd(4*j+4);
+            if (j==0) {
+                hPoint_XvsZ[i]->Draw("P");
+                hPoint_XvsZ[i]->SetMarkerStyle(kCircle);
+            } else {
+                hPoint_YvsZ[i]->Draw("P");
+                hPoint_YvsZ[i]->SetMarkerStyle(kCircle);
+            }
+
+            // cout << v_segments[i].size() << " segments and " << v_extrapolations[i].size() << " extrapolations for event " << i << endl;
+            unsigned int NSegments = v_segments[i].size();
+            unsigned int NExtrapolations = v_extrapolations[i].size();
+
+            for (unsigned int k = 0; k < NSegments; k++) {
+                if (j==0){
+                    v_segments[i][k]->segment_path_x->Draw("SAME");
+                    v_segments[i][k]->segment_path_x->SetLineStyle(2);
+                    v_segments[i][k]->segment_path_x->SetLineWidth(1);
+                    v_segments[i][k]->segment_path_x->SetLineColor(kRed);
+                }
+                else {
+                    v_segments[i][k]->segment_path_y->Draw("SAME");
+                    v_segments[i][k]->segment_path_y->SetLineStyle(2);
+                    v_segments[i][k]->segment_path_y->SetLineWidth(1);
+                    v_segments[i][k]->segment_path_y->SetLineColor(kRed);
+                }
+            }
+
+            for (unsigned int k = 0; k < NExtrapolations; k++) {
+                if (j==0) {
+                    v_extrapolations[i][k]->extrapolation_path_x->Draw("SAME");
+                    v_extrapolations[i][k]->extrapolation_path_x->SetLineStyle(3);
+                    v_extrapolations[i][k]->extrapolation_path_x->SetLineWidth(1);
+                    v_extrapolations[i][k]->extrapolation_path_x->SetLineColor(kBlack);
+                }
+                else {
+                    v_extrapolations[i][k]->extrapolation_path_y->Draw("SAME");
+                    v_extrapolations[i][k]->extrapolation_path_y->SetLineStyle(3);
+                    v_extrapolations[i][k]->extrapolation_path_y->SetLineWidth(1);
+                    v_extrapolations[i][k]->extrapolation_path_y->SetLineColor(kBlack);
+                }
+            }
+        }
+        cout << "Drawing event " << i << " of " << NEventsMonitor << endl;
+        c->Print(pdfFileName);
+    }
+
+    c->Print(pdfFileName + "]");
+    f->Close();
+
+}
