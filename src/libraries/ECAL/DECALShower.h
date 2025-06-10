160dd56922ccb2dc4126f75cb2ddec2dc84c8234
staylorjlab <staylor@ifarm2402.jlab.org>
2025-06-10 11:13:01 -0400

    Add E1/E9 and E9/E25 to printout

--- src/libraries/ECAL/DECALShower.h
+++ src/libraries/ECAL/DECALShower.h
@@ -61,6 +61,8 @@ struct DECALShower : public JObject {
   
   void Summarize(JObjectSummary& summary) const override {
     summary.add(E, "E(GeV)", "%f");
+    summary.add(E1E9, "E1/E9", "%f");
+    summary.add(E9E25, "E9/E25", "%f");
     summary.add(t, "t(ns)", "%f");
     summary.add(pos.x(), "x(cm)", "%f");
     summary.add(pos.y(), "y(cm)", "%f");
