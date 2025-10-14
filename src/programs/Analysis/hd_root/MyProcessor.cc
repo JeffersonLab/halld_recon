a615fa06ef0d90d1470ea9d702f364b9e572ebc0
raiqarasool <rasoolraiqa@gmail.com>
2025-10-14 12:16:42 -0400

    Fix #1054: Missing histograms after upgrading from ROOT 6.24 to 6.32

--- src/programs/Analysis/hd_root/MyProcessor.cc
+++ src/programs/Analysis/hd_root/MyProcessor.cc
@@ -82,6 +82,13 @@ void MyProcessor::Init()
 //------------------------------------------------------------------
 void MyProcessor::BeginRun(const std::shared_ptr<const JEvent>& event)
 {
+	// Manually set gDirectory to ROOTfile because its value is not propagated
+	// from the main thread (which calls ::Init) to the thread that runs ::BeginRun.
+	// Setting it here ensures that all subsequent plugins see the correct gDirectory,
+	// since their ::BeginRun methods are called after this point. 
+	// This issue appeared after upgrading from ROOT 6.24.04 to 6.32.08.
+	gDirectory = ROOTfile;
+	
 	vector<string> factory_names;
 	for (JFactory* factory : event->GetFactorySet()->GetAllFactories()) {
 		factory_names.push_back(factory->GetFactoryName());
