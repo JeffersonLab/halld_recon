e103b6798fe0bede4bbfaa9f73e5c6b526b3ced5
js231 <js23g@fsu.edu>
2026-03-26 22:09:48 -0400

    add ostream customization

--- src/libraries/KINFITTER/DKinFitUtils.h
+++ src/libraries/KINFITTER/DKinFitUtils.h
@@ -49,11 +49,13 @@ class DKinFitUtils //contains pure-virtual functions: cannot directly instantiat
 		bool Get_LinkVerticesFlag(void) const{return dLinkVerticesFlag;}
 		bool Get_DebugLevel(void) const{return dDebugLevel;}
 		bool Get_UpdateCovarianceMatricesFlag(void) const{return dUpdateCovarianceMatricesFlag;}
+		ostream& Get_DebugStream(void) const { return *dDebugStream; }
 
 		//SET CONTROL
 		void Set_LinkVerticesFlag(bool locLinkVerticesFlag){dLinkVerticesFlag = locLinkVerticesFlag;}
 		void Set_DebugLevel(int locDebugLevel){dDebugLevel = locDebugLevel;}
 		void Set_UpdateCovarianceMatricesFlag(bool locUpdateCovarianceMatricesFlag){dUpdateCovarianceMatricesFlag = locUpdateCovarianceMatricesFlag;}
+		void Set_DebugStream(ostream* out_stream) { if (out_stream) { dDebugStream = out_stream; } }
 
 		//GET INPUT FROM OUTPUT
 		shared_ptr<DKinFitParticle> Get_InputKinFitParticle(const shared_ptr<DKinFitParticle>& locKinFitParticle) const;
@@ -144,6 +146,7 @@ class DKinFitUtils //contains pure-virtual functions: cannot directly instantiat
 		bool dLinkVerticesFlag;
 		int dDebugLevel;
 		bool dUpdateCovarianceMatricesFlag;
+		ostream* dDebugStream = &cout;
 
 		shared_ptr<DResourcePool<DKinFitChainStep>> dResourcePool_KinFitChainStep;
 		shared_ptr<DResourcePool<DKinFitChain>> dResourcePool_KinFitChain;
